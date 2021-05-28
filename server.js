adf4835200d03473eb893f2e6bb4dc14cf64e695
shannonguyot <shannonguyot97@gmail.com>
2021-05-28 11:39:23 -0400

    adding env var

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ function sleep(ms) {
 
 async function main() {
   while(true) {
-    console.log('Containers rule!');
+    console.log('Containers rule, ' + process.env.PLURAL_NOUN + ' drool!');
     await sleep(5000);
   }
 }
